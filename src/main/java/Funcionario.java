d19b88f473dd673704454974161db3fcf2dbd3ed
Bebs Fiorillo <estevaovrb2014@gmail.com>
2024-12-12 14:25:31 -0300

    config inicial

---
+++ src/main/java/Funcionario.java
@@ -0,0 +1,10 @@
+public class Funcionario {
+    private String nome;
+    private float salarioBruto;
+    private float totalAcrescimos;
+    private float totalDescontos;
+
+    public String getNome(){
+        return this.nome;
+    }
+}
