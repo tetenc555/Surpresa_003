b8b23f81f9669f625092440ac3d3866949fcbaa5
Bebs Fiorillo <estevaovrb2014@gmail.com>
2024-12-12 14:29:24 -0300

    get set e constructor prontos

--- src/main/java/Funcionario.java
+++ src/main/java/Funcionario.java
@@ -4,7 +4,54 @@ public class Funcionario {
     private float totalAcrescimos;
     private float totalDescontos;
 
-    public String getNome(){
-        return this.nome;
+    public Funcionario(String nome, float salarioBruto, float totalAcrescimos, float totalDescontos) {
+        this.nome = nome;
+        setSalarioBruto(salarioBruto);
+        setTotalAcrescimos(totalAcrescimos);
+        setTotalDescontos(totalDescontos);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getSalarioBruto() {
+        return salarioBruto;
+    }
+
+    public void setSalarioBruto(float salarioBruto) {
+        if (salarioBruto < 0) {
+            throw new IllegalArgumentException("Salario invalido");
+        }
+        if (salarioBruto < 1_412.00F) {
+            throw new IllegalArgumentException("Salario abaixo do minimo");
+        }
+        this.salarioBruto = salarioBruto;
+    }
+
+    public float getTotalAcrescimos() {
+        return totalAcrescimos;
+    }
+
+    public void setTotalAcrescimos(float totalAcrescimos) {
+        if (totalAcrescimos < 0) {
+            throw new IllegalArgumentException("Total de acrescimos invalido");
+        }
+        this.totalAcrescimos = totalAcrescimos;
+    }
+
+    public float getTotalDescontos() {
+        return totalDescontos;
+    }
+
+    public void setTotalDescontos(float totalDescontos) {
+        if (totalDescontos < 0) {
+            throw new IllegalArgumentException("Total de descontos invalido");
+        }
+        this.totalDescontos = totalDescontos;
     }
 }
